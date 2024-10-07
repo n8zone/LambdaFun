2292026a7590300a6e28b414db2db573c37db689
Nathan <92413483+n8zone@users.noreply.github.com>
2024-10-07 13:11:09 -0400

    Added three input AND

--- operators.py
+++ operators.py
@@ -3,6 +3,7 @@ from booleans import *
 # LOGICAL OPERATORS #
 LOG_NOT = lambda b: b(FALSE)(TRUE)
 LOG_AND = lambda b1: lambda b2: b1(b2)(FALSE)
+LOG_TRAND = lambda b1: lambda b2: lambda b3: b1(b2(b3)(FALSE))(FALSE)
 LOG_NAND = lambda b1: lambda b2: LOG_NOT(LOG_AND(b1)(b2))
 LOG_OR = lambda b1: lambda b2: b1(b2)(TRUE)
 LOG_NOR = lambda b1: lambda b2: LOG_NOT(LOG_OR(b1)(b2))
